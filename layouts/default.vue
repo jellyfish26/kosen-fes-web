77ef89cc2ee3506d5a9a472aad8c242ab9487328
jellyfish26 <jellyfish0162@gmail.com>
2020-10-23 18:41:12 +0900

    add navigation and app-bar

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,12 +2,24 @@
   <v-app dark>
     <v-navigation-drawer
       v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
+      right
       fixed
+      width="300"
+      :src="navigation_image"
       app
     >
       <v-list>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title class="title">
+              第54回
+              <h3> 奈良高専祭 </h3>
+            </v-list-item-title>
+            <v-list-item-subtitle>
+              「LINK ~繋がる，広がる，最先端！~」
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
         <v-list-item
           v-for="(item, i) in items"
           :key="i"
@@ -15,6 +27,7 @@
           router
           exact
         >
+          
           <v-list-item-action>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-action>
@@ -25,63 +38,25 @@
       </v-list>
     </v-navigation-drawer>
     <v-app-bar
-      :clipped-left="clipped"
+      v-if="this.$vuetify.breakpoint.width < 1264"
+      clipped-right
       fixed
+      absolute
+      :src="navigation_image"
       app
     >
+      <v-spacer/>
+      <h3> 第54回奈良高専祭 </h3>
+      <v-spacer/>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
     </v-app-bar>
     <v-main>
       <v-container>
         <nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-footer
-      :absolute="!fixed"
-      app
+      absolute
     >
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
@@ -89,29 +64,29 @@
 </template>
 
 <script>
+import Vuetify from 'vuetify'
+
 export default {
   data () {
     return {
       clipped: false,
-      drawer: false,
       fixed: false,
+      drawer: false,
+      navigation_image: require('@/assets/retina_wood.png'),
       items: [
         {
-          icon: 'mdi-apps',
-          title: 'Welcome',
+          icon: 'mdi-home',
+          title: 'トップページ',
           to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
         }
       ],
-      miniVariant: false,
       right: true,
-      rightDrawer: false,
       title: 'Vuetify.js'
     }
+  },
+  mounted() {
+    this.drawer = this.$vuetify.breakpoint.width >= 1264
   }
+  
 }
 </script>
