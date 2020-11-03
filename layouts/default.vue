a57e11cc92a8984f5841dbb9a36ed03b60078d97
jellyfish26 <jellyfish0162@gmail.com>
2020-11-03 17:12:40 +0900

    PC site is left drawer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,7 @@
 <template>
   <v-app dark>
     <v-navigation-drawer
+      v-if="this.$vuetify.breakpoint.width < 1264"
       v-model="drawer"
       right
       fixed
@@ -8,50 +9,17 @@
       :src="navigation_image"
       app
     >
-      <v-container fluid>
-        <v-list>
-          <v-list-item>
-            <v-list-item-content>
-              <v-list-item-title class="title">
-                第54回
-                <h3> 奈良高専祭 </h3>
-              </v-list-item-title>
-              <v-list-item-subtitle>
-                LINK ~繋がる，広がる，最先端！~
-              </v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item
-            v-for="(item, i) in items"
-            :key="i"
-            :to="item.to"
-            router
-            exact
-          >
-
-            <v-list-item-action>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title v-text="item.title" />
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-        <v-layout class="my-10" style="height: 100%" justify-center align-end>
-          <v-btn
-            class="mx-2"
-            fab
-            dark
-            color="#1DA1F2"
-            href="https://twitter.com/nara_kousensai"
-            target="_blank"
-          >
-            <v-icon>
-              mdi-twitter
-            </v-icon>
-          </v-btn>
-        </v-layout>
-      </v-container>
+      <NavigateDrawer />
+    </v-navigation-drawer>
+    <v-navigation-drawer
+      v-else
+      v-model="drawer"
+      fixed
+      width="300"
+      :src="navigation_image"
+      app
+    >
+      <NavigateDrawer />
     </v-navigation-drawer>
     <v-app-bar
       v-if="this.$vuetify.breakpoint.width < 1264"
@@ -75,41 +43,18 @@
 
 <script>
 import Vuetify from 'vuetify'
+import NavigateDrawer from '@/components/NavigateDrawer'
 
 export default {
+  components: {
+    NavigateDrawer
+  },
   data () {
     return {
       clipped: false,
       fixed: false,
       drawer: false,
       navigation_image: require('@/assets/retina_wood.png'),
-      items: [
-        {
-          icon: 'mdi-home',
-          title: 'トップページ',
-          to: '/'
-        },
-        {
-          icon: 'mdi-magnify',
-          title: '実行委員会より',
-          to: '/about'
-        },
-        {
-          icon: 'mdi-biathlon',
-          title: '部活科展',
-          to: '/club'
-        },
-        {
-          icon: 'mdi-school',
-          title: '学科展',
-          to: '/course'
-        },
-        {
-          icon: 'mdi-animation-play',
-          title: '企画(ライブ配信)',
-          to: '/live'
-        }
-      ],
       right: true,
       title: 'Vuetify.js'
     }
