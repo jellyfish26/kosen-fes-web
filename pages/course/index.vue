561c27b911887e67115ef745450efd7e9678917d
jellyfish26 <jellyfish0162@gmail.com>
2020-11-04 01:26:45 +0900

    add course page

--- pages/course/index.vue
+++ pages/course/index.vue
@@ -2,19 +2,22 @@
   <v-app class="course-page">
     <v-container>
       <v-row no-gutters>
-        <v-col class="pa-2" cols="12">
-          <p class="display-2" align="center"> Coming Soon... </p>
-        </v-col>
-        <v-col class="pa-2" cols="12">
-          <h1 align="center"> 公開までしばらくお待ちください。 </h1>
-        </v-col>
-        <v-col class="pa-2" cols="12">
-          <div align="center">
-            <nuxt-link class="subtitle" to="/"> トップページへ戻る。 </nuxt-link>
-          </div>
+        <v-col class="py-12" cols="12">
+          <h1 align="center"> 学科展 </h1>
         </v-col>
       </v-row>
-      
+    </v-container>
+    <v-container style="width: 85%">
+      <v-row>
+        <v-col class="my-6" v-for="item in course" :key="item.name">
+          <CoursePanel
+            :title="item.name"
+            :description="item.description"
+            :url="item.url"
+            :rgb="item.rgb"
+          />
+        </v-col>
+      </v-row>
     </v-container>
   </v-app>
 </template>
@@ -24,3 +27,39 @@
   background: url('~assets/ep_naturalwhite.png') fixed repeat round;
 }
 </style>
+
+<script>
+export default {
+  data() {
+    return {
+      course: [
+        {
+          name: "機械工学科",
+          description: "nothing",
+          rgb: "#ff7a7a"
+        },
+        {
+          name: "電気工学科",
+          description: "電気科展では、これまでに展示してきた展示品に加え、新しく展示する企画もあり、いろとりどりの展示品が並ぶ科展となっております。特設HPと併せて、是非ご覧ください！！！",
+          rgb: "#ffe77a"
+        },
+        {
+          name: "電子制御工学科",
+          description: "課題解決型実験の動画と電子工作",
+          rgb: "#7fff7a"
+        },
+        {
+          name: "情報工学科",
+          description: "みんなで遊ぶ放置ゲーム（鹿煎餅増やしゲーム）AIと対戦，カスタム可能オセロ",
+          rgb: "#7ac1ff"
+        },
+        {
+          name: "物質科学工学科",
+          description: "いろいろな実験を見てみよう！楽しい実験がいっぱい(^^♪ 分かりやすい解説付きです．",
+          rgb: "#9e7aff"
+        }
+      ]
+    }
+  }
+}
+</script>
